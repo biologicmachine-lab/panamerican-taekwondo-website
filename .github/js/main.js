78c8b4d0d3f9b45fb147d038fb92f6a5c0c21e4c
biologicmachine-lab <bio.logic.machine@gmail.com>
2025-12-08 09:44:50 -0500

    Delete .github directory

--- .github/js/main.js
+++
@@ -1,7 +0,0 @@
-// This file contains the JavaScript code for the website, handling interactivity and functionality.
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Website is ready!');
-
-    // Add your interactivity code here
-});
